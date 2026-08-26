73d2b462f082a13ea10b520633a74d638b0b1310
cabravom1 <ca.bravom1@uniandes.edu.co>
2026-08-26 15:10:21 -0500

    Realese Clase

--- DataStructures/Queue/queue.py
+++ DataStructures/Queue/queue.py
@@ -1,6 +1,29 @@
-def new_queue() 
-def enqueue()
-def dequeue()
-def peek() 
-def is_empty()
-def size()
+from DataStructures.List import single_linked_list as lt
+
+
+def new_queue():
+    queue = lt.new_list()
+    return queue
+
+
+def enqueue(my_queue, element):
+    lt.add_last(my_queue, element)
+    return my_queue
+
+
+def dequeue(my_queue):
+    element = lt.remove_first(my_queue)
+    return element
+
+
+def peek(my_queue):
+    element = lt.first_element(my_queue)
+    return element
+
+
+def is_empty(my_queue):
+    return lt.is_empty(my_queue)
+
+
+def size(my_queue):
+    return lt.size(my_queue)
